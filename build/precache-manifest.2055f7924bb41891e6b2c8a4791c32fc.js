c304f2b47332a7816cfcb1acfe0d33ef9abb9c32
james061103jnt <std60010912540@acc.msu.ac.th>
2020-12-20 00:59:31 +0700

    com

---
+++ build/precache-manifest.2055f7924bb41891e6b2c8a4791c32fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd563117c79f3bdc41681d32e84bf612",
+    "url": "/react_crud_hooks/index.html"
+  },
+  {
+    "revision": "7a5c92eaa5a59f194df8",
+    "url": "/react_crud_hooks/static/css/main.f4434642.chunk.css"
+  },
+  {
+    "revision": "c7fb0eef422a3b9048c9",
+    "url": "/react_crud_hooks/static/js/2.83cfcb70.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react_crud_hooks/static/js/2.83cfcb70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a5c92eaa5a59f194df8",
+    "url": "/react_crud_hooks/static/js/main.deacb642.chunk.js"
+  },
+  {
+    "revision": "60c21fa7d75bdfa7ceff",
+    "url": "/react_crud_hooks/static/js/runtime-main.a73aecfa.js"
+  }
+]);
